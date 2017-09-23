b501100ae95db194c4c607d523d4d7280ef9cdb0
Bryan Wong <bryanwong@Bryans-MBP.charter.com>
2017-09-23 00:30:08 -0700

    fixed projects page slide out

--- js/script.js
+++ js/script.js
@@ -4,10 +4,12 @@ var projectsPageOn = 0;
 function toggleProjectsPage(){
 	if (!projectsPageOn){
 		projectsPage.style.left = 0;
+		projectsPage.style.opacity = 1;
 		projectsPageOn = 1;
 	}
 	else{
 		projectsPage.style.left = -100 + "%";
+		projectsPage.style.opacity = 0;
 		projectsPageOn = 0;
 	}
 }
