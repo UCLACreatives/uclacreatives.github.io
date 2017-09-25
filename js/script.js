055d53e6e439b6755b9adc6f16d139e34c09620e
Michael Yu <michael.yu2016@gmail.com>
2017-09-25 01:23:23 -0700

    project page smooth scroll + styles

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 var projectsPage = document.getElementById('projectsPage');
 var projectsPageOn = 0;
 var ui = document.getElementsByClassName('ui');
+var container = document.getElementById('container');
 var about = document.getElementById('about');
+var who = document.getElementById('who');
 var projectsLink = document.getElementById('projectsLink');
+var years = document.getElementsByClassName('year');
 
 function toggleProjectsPage(){
 	var bars = projectsLink.getElementsByClassName('plus')[0].getElementsByTagName('div');
@@ -13,6 +16,9 @@ function toggleProjectsPage(){
 		projectsLink.style.color = '#F8FBFC';
 		bars[0].style.backgroundColor = '#F8FBFC';
 		bars[1].style.backgroundColor = '#F8FBFC';
+		//hide the about and who page
+		about.style.display = 'none';
+		who.style.display = 'none';
 	}
 	else{
 		projectsLink.style.color = '#222428';
@@ -21,6 +27,9 @@ function toggleProjectsPage(){
 		projectsPage.style.left = -100 + "%";
 		projectsPage.style.opacity = 0;
 		projectsPageOn = 0;
+		//hide the about and who page
+		about.style.display = 'flex';
+		who.style.display = 'flex';
 	}
 }
 
@@ -81,18 +90,22 @@ $(document).ready(function(){
 	  var target = this.hash;
 	  var $target = $(target);
 
-	  $('html, body').stop().animate({
+	  $('#projects').stop().animate({
 	      'scrollTop': $target.offset().top
-	  }, 600, 'swing', function () {
+	  }, 400, 'swing', function () {
 	      window.location.hash = target;
 	  });
 	});
-
-	$(".enter").on('click', function() {
-	  $('html, body').stop().animate({
-	      'scrollTop': $("#about").offset().top
-	  }, 600, 'swing', function () {
-	      window.location.hash = ("#about");
-	  });
+	$('.projectsYear').click(function(){
+		$(this).find(".projectsListContainer ul li a").removeClass('gray').addClass('white');
+     	$('.projectsYear').not(this).each(function(){
+         	$(this).find('.projectsListContainer ul li a').removeClass('white').addClass('gray');
+         	if($(this).index() == 0) {
+         		$('.2016').slideUp();
+         		console.log('yo');
+         	}else{
+         		$('.2017').slideUp();
+         	}
+    	});
 	})
 })
